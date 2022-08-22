8a2c0f9c84a1e84e93c10f6731e02f1708e31f5a
pbolotin <Pavel.Bolotin@gmail.com>
2022-08-22 11:37:37 +0700

    Create first version of sorting function

--- 01_selectsort.py
+++ 01_selectsort.py
@@ -33,7 +33,24 @@ def read_file_into_enum_list(filename):
     except Exception:
         eprint("Fatal error with code 0001")
         exit()
-    
+        
+def selection_sort_enum_list_and_comment_steps(enum_list, order="a"):
+    for step in range(1, len(enum_list)):
+        eprint(f"Step {step}: search value less than first in the right part")
+        left_idx = step - 1
+        for right_idx in range(left_idx + 1, len(enum_list)):
+            if order == "a":
+                if enum_list[left_idx][1] > enum_list[right_idx][1]:
+                    eprint(f"SWAP:{enum_list[left_idx][0]},{enum_list[right_idx][0]}")
+                    swap = enum_list[left_idx]
+                    enum_list[left_idx] = enum_list[right_idx]
+                    enum_list[right_idx] = swap
+            else:
+                if enum_list[left_idx][1] < enum_list[right_idx][1]:
+                    eprint(f"SWAP:{enum_list[left_idx][0]},{enum_list[right_idx][0]}")
+                    swap = enum_list[left_idx]
+                    enum_list[left_idx] = enum_list[right_idx]
+                    enum_list[right_idx] = swap
 
 if __name__ == "__main__":
     eprint("01_selectsort")
@@ -44,5 +61,7 @@ if __name__ == "__main__":
         print_help()
         exit()
     
-    lst = read_file_into_enum_list(sys.argv[1])
-    eprint(lst)
+    enm_lst = read_file_into_enum_list(sys.argv[1])
+    eprint(enm_lst)
+    selection_sort_enum_list_and_comment_steps(enm_lst)
+    eprint(enm_lst)
