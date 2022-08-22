81d836f72c0fc5e07fd597074dc1c7d09c14b83a
pbolotin <Pavel.Bolotin@gmail.com>
2022-08-22 14:30:51 +0700

    Create and change the swap mechanics in the sort function and upgrade outputing of the sort process and its result

--- 01_selectsort.py
+++ 01_selectsort.py
@@ -28,30 +28,46 @@ def check_file(filename_to_check):
 def read_file_into_enum_list(filename):
     try:
         f = open(filename, "r")
-        result = [(itr, item) for itr, item in enumerate(f)]
+        result = [(itr, item.rstrip('\n')) for itr, item in enumerate(f)]
         return result
-    except Exception:
+    except OSError:
         eprint("Fatal error with code 0001")
         exit()
+
+def show_input(enum_list):
+    eprint("Input:")
+    [eprint(f"[{n}]:{s}") for n, s in enum_list]
         
 def selection_sort_enum_list_and_comment_steps(enum_list, order="a"):
     for step in range(1, len(enum_list)):
-        eprint(f"Step {step}: search value less than first in the right part")
         left_idx = step - 1
+        eprint(f"Step {step}: try to swap between rightest value in the left part and min(max) value in the right part")
+        eprint([t[0] for t in enum_list[:left_idx+1]],"|",[t[0] for t in enum_list[left_idx+1:]])
+        swap_candidate = left_idx
         for right_idx in range(left_idx + 1, len(enum_list)):
-            if order == "a":
-                if enum_list[left_idx][1] > enum_list[right_idx][1]:
-                    eprint(f"SWAP:{enum_list[left_idx][0]},{enum_list[right_idx][0]}")
-                    swap = enum_list[left_idx]
-                    enum_list[left_idx] = enum_list[right_idx]
-                    enum_list[right_idx] = swap
-            else:
-                if enum_list[left_idx][1] < enum_list[right_idx][1]:
-                    eprint(f"SWAP:{enum_list[left_idx][0]},{enum_list[right_idx][0]}")
-                    swap = enum_list[left_idx]
-                    enum_list[left_idx] = enum_list[right_idx]
-                    enum_list[right_idx] = swap
+            if order == "a" and enum_list[swap_candidate][1] > enum_list[right_idx][1]:
+                eprint(f"SWAP CANDIDATE:{left_idx}:{enum_list[left_idx][0]},{right_idx}:{enum_list[right_idx][0]}")
+                swap_candidate = right_idx
+            elif order != "a" and enum_list[swap_candidate][1] < enum_list[right_idx][1]:
+                eprint(f"SWAP CANDIDATE:{left_idx}:{enum_list[left_idx][0]},{right_idx}:{enum_list[right_idx][0]}")
+                swap_candidate = right_idx
+                
+        if swap_candidate != left_idx:
+            eprint(f"SWAP {left_idx}:{enum_list[left_idx][0]}, {swap_candidate}:{enum_list[swap_candidate][0]}")
+            swap = enum_list[left_idx]
+            enum_list[left_idx] = enum_list[swap_candidate]
+            enum_list[swap_candidate] = swap
+            
+        eprint()
+            
+    eprint("RESULT:", [t[0] for t in enum_list])
 
+def output_result(enum_list):
+    for n, s in enum_list:
+        if sys.stdout.isatty():
+            eprint(f"[{n}]:", end="", flush=True)
+        print(s, flush=True)
+    
 if __name__ == "__main__":
     eprint("01_selectsort")
     if not arguments_validation():
@@ -62,6 +78,6 @@ if __name__ == "__main__":
         exit()
     
     enm_lst = read_file_into_enum_list(sys.argv[1])
-    eprint(enm_lst)
+    show_input(enm_lst)
     selection_sort_enum_list_and_comment_steps(enm_lst)
-    eprint(enm_lst)
+    output_result(enm_lst)
